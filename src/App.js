ee16fe9c82a510b9ed4354f9f8ce7febcea94c64
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-25 16:12:41 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>React app Deployment Free hosting</h1>
+          <p>if you like this video click on below and subscribe us</p>
+          <button><a href="https://www.youtube.com/@JavaScriptFolks/videos">Click me!</a></button>
     </div>
   );
 }
