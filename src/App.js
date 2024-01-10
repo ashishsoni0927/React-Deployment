743d8639e5a253473425de0cb36df9ee24c13b5e
ashishsoni0927 <73069751+ashishsoni0927@users.noreply.github.com>
2024-01-10 15:56:05 +0530

    Update App.js
    link updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
           <h1>React app Deployment Free hosting</h1>
           <p>if you like this video click on below and subscribe us</p>
-          <button><a href="https://www.youtube.com/@JavaScriptFolks/videos">Click me!</a></button>
           <h1>This is last video of this playlist Ashish soni test</h1>
     </div>
   );
