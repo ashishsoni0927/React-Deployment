a73d08b7f98e7f47cc898019dcd6485f6269f21a
ashishsoni0927 <73069751+ashishsoni0927@users.noreply.github.com>
2024-01-10 18:37:23 +0530

    Update App.js
    demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
           <h1>React app Deployment Free hosting</h1>
-          <p>if you like this </p>
+          <p>this is my demo code </p>
           <h1>This is last video of this playlist Ashish soni test</h1>
     </div>
   );
