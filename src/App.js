00b845433a679e2836007e0a963cd31cbbef345f
Ashish <ashish@gmail.com>
2024-01-10 21:35:29 +0530

    update vercel.yaml file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
           <h1>React app Deployment Free hosting</h1>
           <p>this is my demo and latest code</p>
-          <h1>This is last video of this playlist Ashish soni test</h1>
+          <h1>Ashish soni test</h1>
     </div>
   );
 }
