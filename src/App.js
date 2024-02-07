789406d8a0f45e600a42e4e8ff3fd94114a080e3
ashishsoni0927 <73069751+ashishsoni0927@users.noreply.github.com>
2024-02-07 17:41:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
           <h1>React app Deployment Free hosting</h1>
-          <p>this is my demo and latest code for ashish branch 2</p>
+          <p>this is my demo and latest code for ashish branch 3</p>
           <h1>Ashish soni test</h1>
     </div>
   );
