4bb85619280ce9d0a0c1d5bbffe6f7924fff3907
ashishsoni0927 <73069751+ashishsoni0927@users.noreply.github.com>
2024-03-15 00:29:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
           <h1>React app Deployment Free hosting</h1>
           <p>this is my demo and latest code for testing for the for prod 14 of feb</p>
-          <h1>Ashish soni test 13/03/2024</h1>
+          <h1>Ashish soni test 15/03/2024</h1>
     </div>
   );
 }
